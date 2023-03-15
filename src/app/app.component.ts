5b2d53087414b2b7c9192d17c9cc56d20883c134
Sivaraman S <sivaramans@MINSS241.local>
2023-03-15 22:47:49 +0530

    integrated to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Item } from './utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  constructor(private afs: AngularFirestore){
+    this.itemsCollection = afs.collection<Item>('items');
+    this.items = this.itemsCollection.valueChanges();
+  }
+
   title = 'ng-fire-dexie';
 
   create(){
+    this.itemsCollection.doc('item2').set({
+      id: '1',
+      name: 'Iphone',
+      quantity: '1',
+      price: 850000
+    })
     console.log("create a firebase collections and add records to it")
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+const firebaseConfig= {
+  apiKey: "AIzaSyAB7CRKN5w6L3s9eX2cQ4Gtjga1Apf5TX0",
+  authDomain: "ng-fire-dexie.firebaseapp.com",
+  projectId: "ng-fire-dexie",
+  storageBucket: "ng-fire-dexie.appspot.com",
+  messagingSenderId: "1072073376593",
+  appId: "1:1072073376593:web:b9ac6d7ead038d22d60970",
+  measurementId: "G-FGGK17P44Z"
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +22,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

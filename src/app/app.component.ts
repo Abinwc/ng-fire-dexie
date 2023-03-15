e7547b011de09372de121a6f6860d016fe0115bf
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-03-15 22:36:31 +0530

    push data to realtime datbse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private adb:AngularFireDatabase){}
   title = 'ng-fire-dexie';
+
+  addItems = new FormGroup({
+    input: new FormControl('',Validators.required),
+  })
+
+
+  pushData(){
+    this.adb.database.ref('/inputs').push(this.addItems.value)
+    // console.log(this.addItems.value)
+  }
+
+  getData(){
+    //to fetch the code
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw_q17_pxrVJztLBZ8FDqTbcQ2pzDXwVM",
+  authDomain: "ng-fire-dexie-4ea74.firebaseapp.com",
+  projectId: "ng-fire-dexie-4ea74",
+  storageBucket: "ng-fire-dexie-4ea74.appspot.com",
+  messagingSenderId: "528541288993",
+  appId: "1:528541288993:web:d97b28b77d72df8887f160"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +20,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
